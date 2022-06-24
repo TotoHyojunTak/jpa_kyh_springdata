df8c3e12c994b18f782d15ccc4eb1d072ed4528e
thj0309 <thj0309@cj.net>
2022-06-24 09:34:18 +0900

    add

---
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package study.datajpa.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m from Member m where m.username = :name")
+    Member findMembers(@Param("name") String username);
+
+    Page<Member> findByAge(int age, Pageable pageable);
+}
